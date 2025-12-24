17760dfce5aee574486da5f25654c892e3856c81
Barun Kumar Mishra <144221171+Barun-Mishra-09@users.noreply.github.com>
2025-12-24 13:30:21 +0530

    Update CORS origin for deployment URL

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(
   cors({ 
-    origin: "http://localhost:5173" || "https://barun-ai-codeguard-hq7r.onrender.com",
+    origin: "http://localhost:5173" || "https://barun-ai-codeguard-r5an.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
